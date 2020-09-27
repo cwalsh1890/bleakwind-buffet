39ee0162c79992d71ae3795d05eca952b2c25e0e
Coby Walsh <cwalsh1890@ksu.edu>
2020-09-27 17:14:12 -0500

    Finshed making base of gui

---
+++ PointOfSale/Entree/SHSkeletonModifiers.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale.Entree {
+	/// <summary>
+	/// Interaction logic for SHSkeletonModifiers.xaml
+	/// </summary>
+	public partial class SHSkeletonModifiers : UserControl {
+		public SHSkeletonModifiers() {
+			InitializeComponent();
+		}
+	}
+}
