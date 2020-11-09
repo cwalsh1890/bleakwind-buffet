462b5c2a94daefef27c43eca04699bbed226743b
Coby Walsh <cwalsh1890@ksu.edu>
2020-11-08 22:08:08 -0600

    finished web milestone 2

--- Data/Menu/Menu.cs
+++ Data/Menu/Menu.cs
@@ -11,6 +11,7 @@ using BleakwindBuffet.Data.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 
 namespace BleakwindBuffet.Data.Menu {
@@ -211,17 +212,47 @@ namespace BleakwindBuffet.Data.Menu {
 			List<IOrderItem> items = new List<IOrderItem>();
 			ArentinoAppleJuice aj = new ArentinoAppleJuice();
 			items.Add(aj);
+			aj = new ArentinoAppleJuice();
+			aj.Size = Size.Medium;
+			items.Add(aj);
+			aj = new ArentinoAppleJuice();
+			aj.Size = Size.Large;
+			items.Add(aj);
 
 			CandlehearthCoffee cc = new CandlehearthCoffee();
+			items.Add(cc);
+			cc = new CandlehearthCoffee();
+			cc.Size = Size.Medium;
+			items.Add(cc);
+			cc = new CandlehearthCoffee();
+			cc.Size = Size.Large;
 			items.Add(cc);
 
 			MarkarthMilk mm = new MarkarthMilk();
 			items.Add(mm);
+			mm = new MarkarthMilk();
+			mm.Size = Size.Medium;
+			items.Add(mm);
+			mm = new MarkarthMilk();
+			mm.Size = Size.Large;
+			items.Add(mm);
 
 			WarriorWater ww = new WarriorWater();
 			items.Add(ww);
+			ww = new WarriorWater();
+			ww.Size = Size.Medium;
+			items.Add(ww);
+			ww = new WarriorWater();
+			ww.Size = Size.Large;
+			items.Add(ww);
 
 			SailorSoda ss = new SailorSoda();
+			items.Add(ss);
+			ss = new SailorSoda();
+			ss.Size = Size.Medium;
+			items.Add(ss);
+			ss = new SailorSoda();
+			ss.Size = Size.Large;
 			items.Add(ss);
 
 			return items;
@@ -239,6 +270,105 @@ namespace BleakwindBuffet.Data.Menu {
 			entrees.AddRange(sides);
 			entrees.AddRange(drinks);
 			return entrees;
+		}
+
+		public static IEnumerable<IOrderItem> FilterByName(IEnumerable<IOrderItem> items, string name)
+        {
+			if (name == null)
+				return items;
+
+			List<IOrderItem> results = new List<IOrderItem>();
+
+			foreach (IOrderItem item in items)
+            {
+				string s = item.ToString();
+				s.ToLower();
+				name.ToLower();
+				if (s.Contains(name))
+					results.Add(item);
+            }
+			return results;
+        }
+
+		public static IEnumerable<IOrderItem> FilterByItemType(IEnumerable<IOrderItem> items, string[] types)
+        {
+			if (types.Length == 0)
+				return items;
+
+			List<IOrderItem> results = new List<IOrderItem>();
+
+			foreach (IOrderItem item in items)
+            {
+				foreach (string type in types)
+                {
+					switch (type)
+                    {
+						case "Entree":
+							if (item is Entree)
+								results.Add(item);
+							break;
+						case "Drink":
+							if (item is Drink)
+								results.Add(item);
+							break;
+						case "Side":
+							if (item is Side)
+								results.Add(item);
+							break;
+					}
+                }
+            }
+			return results;
+		}
+
+		public static IEnumerable<IOrderItem> FilterByCalories(IEnumerable<IOrderItem> items, uint? min, uint? max)
+		{
+			if (min == null && max == null)
+			{
+				return items;
+			}
+			else if (min == null && max != null)
+			{
+				min = 0;
+			}
+			else if (min != null && max == null)
+			{
+				max = 9999;
+			}
+
+			List<IOrderItem> results = new List<IOrderItem>();
+
+			foreach (IOrderItem item in items)
+			{
+				if (item.Calories >= min && item.Calories <= max)
+					results.Add(item);
+			}
+			return results;
+		}
+
+		public static IEnumerable<IOrderItem> FilterByPrice(IEnumerable<IOrderItem> items, double? min, double? max)
+		{
+			if (min == null && max == null)
+			{
+				return items;
+			}
+			else if (min == null && max != null)
+			{
+				min = 0;
+			}
+			else if (min != null && max == null)
+			{
+				max = 9999;
+			}
+
+			List<IOrderItem> results = new List<IOrderItem>();
+
+			foreach (IOrderItem item in items)
+			{
+				if (item.Price >= min && item.Price <= max)
+					results.Add(item);
+			}
+			return results;
 		}
 	}
 }
--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BleakwindBuffet.Data.Menu;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -12,6 +13,19 @@ namespace Website.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public IEnumerable<IOrderItem> Entrees { get; set; } 
+        public IEnumerable<IOrderItem> Drinks { get; set; }
+        public IEnumerable<IOrderItem> Sides { get; set; }
+
+        public string SearchTerms { get; set; }
+        public string[] ItemType { get; set; }
+
+        public uint? CalorieMin { get; set; }
+        public uint? CalorieMax { get; set; }
+
+        public double? PriceMin {get; set; }
+        public double? PriceMax { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -19,7 +33,54 @@ namespace Website.Pages
 
         public void OnGet()
         {
+            Entrees = Menu.Entrees();
+            Sides = Menu.Sides();
+            Drinks = Menu.SmallDrinks();
 
+            SearchTerms = Request.Query["SearchTerms"];
+            ItemType = Request.Query["ItemType"];
+
+            string s = Request.Query["CalorieMin"].ToString();
+            if (s.Length > 0) 
+                CalorieMin = uint.Parse(s);
+            else
+                CalorieMin = null;
+
+            s = Request.Query["CalorieMax"].ToString();
+            if (s.Length > 0)
+                CalorieMax = uint.Parse(s);
+            else
+                CalorieMax = null;
+
+            s = Request.Query["PriceMin"].ToString();
+            if (s.Length > 0)
+                PriceMin = double.Parse(s);
+            else
+                PriceMin = null;
+
+            s = Request.Query["PriceMax"].ToString();
+            if (s.Length > 0)
+                PriceMax = double.Parse(s);
+            else
+                PriceMax = null;
+
+            
+            Entrees = Menu.FilterByName(Entrees, SearchTerms);
+            Entrees = Menu.FilterByItemType(Entrees, ItemType);
+            Entrees = Menu.FilterByCalories(Entrees, CalorieMin, CalorieMax);
+            Entrees = Menu.FilterByPrice(Entrees, PriceMin, PriceMax);
+            
+            Sides = Menu.FilterByName(Sides, SearchTerms);
+            Sides = Menu.FilterByItemType(Sides, ItemType);
+            Sides = Menu.FilterByCalories(Sides, CalorieMin, CalorieMax);
+            Sides = Menu.FilterByPrice(Sides, PriceMin, PriceMax);
+
+            Drinks = Menu.FilterByName(Drinks, SearchTerms);
+            Drinks = Menu.FilterByItemType(Drinks, ItemType);
+            Drinks = Menu.FilterByCalories(Drinks, CalorieMin, CalorieMax);
+            Drinks = Menu.FilterByPrice(Drinks, PriceMin, PriceMax);
+
+            
         }
     }
 }
